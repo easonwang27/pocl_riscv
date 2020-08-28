c8d6a877093b572051fbbadefe534f316d315851
eawang <easonwang27@163.com>
2020-08-27 21:40:56 -0400

    add lib

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@ cd build
 
 cmake  ../ -G "Unix Makefiles" -DENABLE_MONTAGE_DEVICES=ON \
 -DPOCL_INSTALL_ICD_VENDORDIR=/etc/OpenCL/vendors \
--DCLANG:FILEPATH=/home/eawang/work/llvm_lib/bin/clang \
--DCLANGXX:FILEPATH=/home/eawang/work/llvm_lib/bin/clang++ \
--DLLVM_AS:FILEPATH=/home/eawang/work/llvm_lib/bin/llvm-as \
--DLLVM_CONFIG:FILEPATH=/home/eawang/work/llvm_lib/bin/llvm-config \
--DLLVM_LINK:FILEPATH=/home/eawang/work/llvm_lib/bin/llvm-link \
--DLLVM_LLC:FILEPATH=/home/eawang/work/llvm_lib/bin/llc \
--DLLVM_LLI:FILEPATH=/home/eawang/work/llvm_lib/bin/lli \
--DLLVM_OPT:FILEPATH=/home/eawang/work/llvm_lib/bin/opt 
+-DCLANG:FILEPATH=/home/eawang/_worklib/bin/clang \
+-DCLANGXX:FILEPATH=/home/eawang/_worklib/bin/clang++ \
+-DLLVM_AS:FILEPATH=/home/eawang/_worklib/bin/llvm-as \
+-DLLVM_CONFIG:FILEPATH=/home/eawang/_worklib/bin/llvm-config \
+-DLLVM_LINK:FILEPATH=/home/eawang/_worklib/bin/llvm-link \
+-DLLVM_LLC:FILEPATH=/home/eawang/_worklib/bin/llc \
+-DLLVM_LLI:FILEPATH=/home/eawang/_worklib/bin/lli \
+-DLLVM_OPT:FILEPATH=/home/eawang/_worklib/bin/opt 
 
 make -j16
 
